118c6ff96c277ca0315f51af01221519f93ee16f
Iljin Teemu Sakari <Teemu.iljin@gmail.com>
2024-12-07 19:32:46 +0200

    fixed pycharm bugs and config, changes to files for manage.py. Better project structure and the end of day 1

--- ai_assistant/projects/models.py
+++ ai_assistant/projects/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField(null=True, blank=True)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
