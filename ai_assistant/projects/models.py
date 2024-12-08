fc94ace4c997de6ba0ce0b3984cb0b572253d557
Iljin Teemu Sakari <Teemu.iljin@gmail.com>
2024-12-08 12:32:38 +0200

    set url, created basic html files and linked everything together. Project view created and small database starter created

--- ai_assistant/projects/models.py
+++ ai_assistant/projects/models.py
@@ -11,3 +11,14 @@ class Project(models.Model):
 
     def __str__(self):
         return self.name
+
+# Task model
+class Task(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='tasks')
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    completed = models.BooleanField(default=False)
+    due_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
