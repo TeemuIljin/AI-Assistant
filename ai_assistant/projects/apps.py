e61605735612761119749ff1c49afa52942aa147
Iljin Teemu Sakari <Teemu.iljin@gmail.com>
2024-12-13 12:44:12 +0200

    Added api features and security measures. pip update too.

--- ai_assistant/projects/apps.py
+++ ai_assistant/projects/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class ProjectsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'projects'
+
+from decouple import config
+
+DEBUG = config('DEBUG', default=False, cast=bool)
+SECRET_KEY = config('SECRET_KEY')
+
